00b6d7f7e353db5c734f1a91823a069bb70af5d8
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-04-11 20:57:52 +0200

    Added attributes to list

--- data_structures/lists/SingleLinkedList.java
+++ data_structures/lists/SingleLinkedList.java
@@ -1,7 +1,17 @@
 package data_structures.lists;
 
-//Single Linked List of generic nodes
+//Single Linked List of generic nodes, with an internal class Node. 
+//T is the generic type the list will contain.
 public class SingleLinkedList<T>{
+    //head is the starting node of the list
+    private Node<T> head = null;
+    //tail is the last node of the list
+    private Node<T> tail = null;
+    //size counts how many elements the list contains
+    private int size = 0;
+
     
+    
+    //internal class
     class Node<T>{}
 }
