9320a15595c5fe99b941c7dfee0232727425544b
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-04-13 00:31:54 +0200

    Added constructors to SingleLinkedList

--- data_structures/lists/SingleLinkedList.java
+++ data_structures/lists/SingleLinkedList.java
@@ -1,20 +1,35 @@
 package data_structures.lists;
 
-//Single Linked List of generic nodes, with a nested cclass Node. 
+//Single Linked List of generic nodes, with a nested class Node. 
 //T is the generic type the list will contain.
 public class SingleLinkedList<T>{
 
-    //head is the starting node of the list
-    private Node<T> head = null;
-    //tail is the last node of the list
-    private Node<T> tail = null;
-    //size counts how many elements the list contains
-    private int size = 0;
+      //head is the starting node of the list
+      private Node<T> head = null;
+      //tail is the last node of the list
+      private Node<T> tail = null;
+      //size counts how many elements the list contains
+       private int size = 0;
 
-    //method which returns whether the list is empty
-    public Boolean isEmpty(){
-        return size==0;
-    }
+      //constructors to build the list:one without arguments, one with the head and one with head and tail
+      public SingleLinkedList();
+      public SingleLinkedList(Node<T> first){
+          head = first;
+      }
+      public SingleLinkedList(Node<T> first, Node<T> last){
+          head = first;
+          tail = last;
+      }
+
+      //method which returns whether the list is empty
+      public boolean IsEmpty(){
+          return size==0;
+      }
+
+     //getter of the list size
+      public int Size(){
+           return size;
+      }
 
 
 
