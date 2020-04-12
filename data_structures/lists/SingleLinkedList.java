c654f202b2245f5beb6d3aca068b1fad56d67a51
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-04-12 18:54:35 +0200

    Corrected grammatical error, added some methods in SingleLinked List

--- data_structures/lists/SingleLinkedList.java
+++ data_structures/lists/SingleLinkedList.java
@@ -1,8 +1,9 @@
 package data_structures.lists;
 
-//Single Linked List of generic nodes, with an internal class Node. 
+//Single Linked List of generic nodes, with a nested cclass Node. 
 //T is the generic type the list will contain.
 public class SingleLinkedList<T>{
+
     //head is the starting node of the list
     private Node<T> head = null;
     //tail is the last node of the list
@@ -10,8 +11,15 @@ public class SingleLinkedList<T>{
     //size counts how many elements the list contains
     private int size = 0;
 
+    //method which returns whether the list is empty
+    public Boolean isEmpty(){
+        return size==0;
+    }
+
+
+
+
     
-    
-    //internal class
+    //nested class
     class Node<T>{}
 }
