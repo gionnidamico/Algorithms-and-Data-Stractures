a67b2156e8c2fa1fabae46c46ca581759a57110c
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-06-06 02:44:10 +0200

    created base files for an array based stack

---
+++ data_structures/stacks/ArrayBasedStack.java
@@ -0,0 +1,7 @@
+package data_structures.stacks;
+import data_structures.stacks.interfaces;
+
+//implementing a stack class based on the TStack interface, using an array as a base structure
+public class ArrayBasedStack<T> implements TStack<T>{
+    
+} 
