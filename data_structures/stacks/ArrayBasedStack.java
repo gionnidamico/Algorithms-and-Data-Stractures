8e89fc794dffdc73b2eb8509c445ac22454dbe64
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-06-07 02:14:35 +0200

    created private attributes and a costructor for the ArrayBased stack

--- data_structures/stacks/ArrayBasedStack.java
+++ data_structures/stacks/ArrayBasedStack.java
@@ -2,6 +2,16 @@ package data_structures.stacks;
 import data_structures.stacks.interfaces;
 
 //implementing a stack class based on the TStack interface, using an array as a base structure
+//note that this implementations makes the size of a new stack not changeable during his lifespan
 public class ArrayBasedStack<T> implements TStack<T>{
-    
+
+    //data in the stack is stored in this array, but not directly accessible from it
+    private T[] dataStored;
+    //index of the element at the top of the array;also not accessible from outside the class 
+    private int topIndex = -1;                       //-1 flags that the stack is empty, and is therefore initialized so
+
+    //constructor which simply creates space where inserting the elements
+    public ArrayBasedStack(int capacity){
+        dataStored = T[] new Object[capacity];       //creates an array of generic Objects and then downcastes to be an array of T elements because T is a generic    
+    }  
 } 
