207ddd6e6a49a878cbed386b3999bc4f768ed930
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-06-12 00:55:36 +0200

    added size and isEmpty methods; corrected nofElements initialization

--- data_structures/queues/ArrayBasedQueue.java
+++ data_structures/queues/ArrayBasedQueue.java
@@ -5,12 +5,22 @@ import data_structures.queues.interfaces;
 public class ArrayBasedQueue<T> implements TQueue<T>{
 
         //counts the number of elements currently in the queue
-        public int nOfElements;
+        public int nOfElements = 0;
         //this is the array used as a base structure for the queue
         public T[] content;
 
         //constructor which creates a new array-based queue with the max size of 'capacity'
         public ArrayBasedQueue(int capacity){
                 content = (T[]) new Object[capacity];
+        }
+
+        //returns the number of elements that are currently in the queue(aka:getter of the queue size)
+        public int size(){
+                return nOfElements;
+        }
+
+        //returns whether the queue is empty
+        public boolean isEmpty(){
+                return (nOfElements == 0);
         }
 }
