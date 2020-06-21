651fc5723d8459a166b39dfb373f69f433a93bbe
gionnidamico <47538301+gionnidamico@users.noreply.github.com>
2020-06-21 21:29:20 +0200

    created the basic position interface + started Tree interface

---
+++ data_structures/trees/interfaces/Tree.java
@@ -0,0 +1,6 @@
+package data_structures.trees.interfaces;
+
+//Generic interface for any tree data structure (particularly, one with any number of children)
+public interface Tree<T>  extends Iterable<T>{
+    
+}
